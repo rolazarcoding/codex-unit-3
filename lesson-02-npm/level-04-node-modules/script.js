e558ccb8a7ba1bc72492b9b536da433374553995
Rolazar <rolazarcoding@gmail.com>
2026-06-17 18:36:04 -0700

    finished level 4

--- lesson-02-npm/level-04-node-modules/script.js
+++ lesson-02-npm/level-04-node-modules/script.js
@@ -1,1 +1,13 @@
-import {} from "node:os";
+import { cpus, totalmem, freemem, machine, platform } from "node:os";
+
+const cpuInfo = cpus();
+const totalmemInfo = totalmem();
+const freememInfo = freemem();
+const machineInfo = machine();
+const platformInfo = platform();
+
+console.log("CPU:", cpuInfo);
+console.log(`Memory: ${totalmemInfo}`);
+console.log(`Free: ${freememInfo}`);
+console.log(`Machine: ${machineInfo}`);
+console.log(`Platform: ${platformInfo}`);
