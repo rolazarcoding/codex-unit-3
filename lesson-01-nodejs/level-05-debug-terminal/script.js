2b3157f77575df40e64e1a70b52d602134d41583
Rolazar <rolazarcoding@gmail.com>
2026-06-17 17:46:21 -0700

    finished level 5

--- lesson-01-nodejs/level-05-debug-terminal/script.js
+++ lesson-01-nodejs/level-05-debug-terminal/script.js
@@ -1,2 +1,5 @@
 console.log("Roses are red.");
 console.log("Violets are blue.");
+console.log(
+  "1. Set a breakpoint.\n2. Choose JavaScript Debug Terminal from the dropdown.\n3. Run the script.\n4. Use the debugger controls to run each line of code.",
+);
