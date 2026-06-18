9646289f2dc3af6ddce3d6385d1534a382b3336b
Rolazar <rolazarcoding@gmail.com>
2026-06-17 17:46:08 -0700

    finished level 3

---
+++ lesson-02-npm/level-03-named-imports/index.js
@@ -0,0 +1,4 @@
+import { message1, message2 } from "./messages.js";
+
+console.log(message1);
+console.log(message2);
---
+++ lesson-02-npm/level-03-named-imports/messages.js
@@ -0,0 +1,5 @@
+export const message1 =
+  "Named exports are exported and defined on the same line.";
+
+export const message2 =
+  "Named imports use curly braces and match the name of the export. Multiple imports from the same file can be separated by commas.";
